663eef0da55cba4eb408bc2ccf37bfd7fe79961f
Chiky <assault108@gmail.com>
2019-04-16 12:34:48 +0300

    Учеба на Udemy

---
+++ less 1/scr.js
@@ -0,0 +1,14 @@
+var money = prompt ( "Ваш бюджет на месяц?", "");
+
+
+let appData = {
+    timeData: prompt ("Введите дату", "YYYY-MM-DD" ),
+    let expenses = {
+
+    },
+    let optionalExpenses = {
+
+    },
+    let income = [],
+    savings6: false
+}
